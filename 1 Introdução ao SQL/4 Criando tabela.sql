642addad01dc9f317fb00caa68dc9bce4a33eaaa
João Estevan Barbosa <85952745+joaoestevanbarbosa@users.noreply.github.com>
2021-10-25 18:05:30 -0300

    Criando tabela.

---
+++ 1 Introdução ao SQL/4 Criando tabela.sql
@@ -0,0 +1,11 @@
+-- CRIAR UMA TABELA
+CREATE TABLE MinhaTabela
+(
+	Codigo	INT NOT NULL, --NÃO PODE SER NULO
+	Texto	VARCHAR(30) NULL, -- é um texto ou string --PODE SER NULÓ
+	Endereco VARCHAR(30) NULL,
+	Endereco1 VARCHAR(30) NULL,
+	Bairro VARCHAR(30) NULL,
+	Cidade VARCHAR(30) NULL,
+	Estado VARCHAR(2) NULL,
+)
